c99c55e676b55523981d8f3cfde4f25fd967b771
Duran <sakalli.duran@gmail.com>
2020-03-09 00:04:05 -0700

    first commit

---
+++ Bluetooth_Paired_Device/src/Bluetooth_Device/Pair_Device.java
@@ -0,0 +1,20 @@
+package Bluetooth_Device;
+
+import java.io.IOException;
+
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+
+public class Pair_Device {
+	public static void main(String[] args) throws IOException {
+		LocalDevice device = LocalDevice.getLocalDevice();
+		
+		RemoteDevice[] remotedevice = device.getDiscoveryAgent().retrieveDevices(DiscoveryAgent.PREKNOWN);
+		
+		for(RemoteDevice d : remotedevice) {
+			System.out.println("Device Name: "+d.getFriendlyName(false));
+			System.out.println("Bluetooth Address: "+d.getBluetoothAddress()+"\n");
+		}
+	}
+}
